17027136f42f21040159ef4acaf8a6f7ce2cd749
anonymous <develop@dev.net>
2020-01-24 12:22:47 +0300

    change file

--- index.php
+++ index.php
@@ -1,9 +1,51 @@
 <?php
 
+$pdo = new PDO ("mysql:host=localhost;dbname=ch35098_githab","ch35098_githab","m0t0r0la");
+$statement = $pdo->prepare("SELECT * FROM tasks");
+      $statement->execute();
+      $tasks=$statement->fetchAll(PDO::FETCH_ASSOC);
+
+//var_dump($tasks);
 ?>
 <DOCTYPE HTML>
 <HTML LANG="RU">
-  
+  <head>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="" crossorigin="anonymous">
+
+  </head>
+  <body>
+  	<div class="container-fluid">
+  		<div class="row">
+  			<div class="col-md-12">
+  				<h1>Tasks</h1>
+  				<a href="create.php" class="btn btn-success">Add tasks</a>
+  				<table class="table">
+  					<thead>
+  						<tr>
+  							<th>id</th>
+  							<th>Title</th>
+  							<td>Action</td>
+  						</tr>
+  					</thead>
+  					<tbody>
+  						   <?php foreach($tasks as $task):?>  
+  						<tr>
+  							
+  						<td><?= $task['id'];?></td>
+  						<td><?= $task['title'];?></td>
+  						<td><a href="show.php?id=<?= $task['id'];?>" class="btn btn-info">Show</a></td>
+  						<td><a href="edit.php?id=<?= $task['id'];?>" class="btn btn-warning">Edit</a></td>
+  						<td><a href="delete.php?id=<?= $task['id'];?>" class="btn btn-danger">Delete</a></td>
+  						
+  						</tr>
+  						<?php endforeach;?>
+  					</tbody>
+  				</table>
+  				</div>
+  		</div>
+  	</div>
+  	
+  </body>
   
   </HTML>
   
