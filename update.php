95c2e0ed661f32419bdac419a9e233acc7bef35b
anonymous <develop@dev.net>
2020-01-26 16:24:21 +0300

    update success!

--- update.php
+++ update.php
@@ -1,13 +1,15 @@
 <?php
 
 $data = [ 'id'=> $_GET['id'],
-         'text'=> $_POST['title'],
-         'name'=> $_POST['content']
-          ];
+         'title'=> $_POST['title'],
+         'content'=> $_POST['content']];
         
-         
+        
 $pdo = new PDO ("mysql:host=localhost;dbname=ch35098_githab","ch35098_githab","m0t0r0la");
-$statement = $pdo->prepare("UPDATE tasks SET title=:title, content=:content WHERE id=:id");
+$sql = "UPDATE  tasks SET title=:title, content=:content  WHERE id=:id";
+$statement = $pdo->prepare($sql);
 $statement->execute($data);
+
 $task = $statement->fetch(PDO::FETCH_ASSOC);
+
 header("Location:/githab/tasks/index.php");
